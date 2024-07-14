384b2afe21b9f27f6ac2bd9abc6b49297f95f5fe
Amaan-Khan14 <amaank14@icloud.com>
2024-07-14 21:00:35 +0530

    Prisma added

---
+++ server/routes/blog.ts
@@ -0,0 +1,8 @@
+// import { Hono } from "hono";
+
+// export const blogrouter = new Hono<{
+//     Bindings: {
+//         DATABASE_URL: string;
+//         JWT_SECRET: string;
+//     }
+// }>()
---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+// import { userRouter } from '../routes/user';
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>()
+
+app.get('/', (c) => {
+  return c.text('Amaan! This side')
+})
+
+// app.route('/user', userRouter)
+
+export default app
