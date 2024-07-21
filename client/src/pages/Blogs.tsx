c8113ac969be62090e814ad22853824eec3f4e0e
Amaan-Khan14 <amaank14@icloud.com>
2024-07-22 00:57:22 +0530

    Skeleton Added

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,13 +1,24 @@
 import { AppBar } from "@/components/ui/Appbar"
 import { BlogCard } from "@/components/ui/blogcard"
 import { useBlogs } from "@/hooks"
-
+import { Skeleton } from "@/components/ui/skeleton"
 
 export const Blogs = () => {
     const { blogs, isLoading } = useBlogs();
 
     if (isLoading) {
-        return <div>Loading...</div>;  // Or any loading indicator
+        return (
+            <div>
+                <AppBar />
+                <div className="flex justify-center">
+                    <div className="w-1/2 mt-8">
+                        {[...Array(5)].map((_, index) => (
+                            <Skeleton key={index} className="w-full h-[200px] bg-stone-100/50 rounded-lg mb-4" />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        )
     }
     return (
         <div>
